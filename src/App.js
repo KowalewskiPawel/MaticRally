7e1dbdce09f9e9ba82eed6597759d243cae84a9e
Pawel Kowalewski <pkowalewski@microstrategy.com>
2022-03-04 21:16:03 +0100

    inital boilerplate

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import Phaser from "phaser";
+import { IonPhaser } from "@ion-phaser/react";
+import { useMoralis } from "react-moralis";
+
+
+const App = () => {
+  const { Moralis, authenticate, logout, isAuthenticated, user } = useMoralis();
+
+  const game = {
+    width: 800,
+    height: 600,
+    type: Phaser.AUTO,
+    physics: {
+      default: "arcade",
+      arcade: {
+        gravity: { y: 300 },
+        debug: false,
+      },
+    },
+    scene: {
+      preload: function () {
+      },
+      create: async function () {
+      },
+      update: async function () {
+      },
+    },
+  };
+
+  if (!isAuthenticated) {
+    return (
+      <div>
+        <button onClick={() => authenticate()}>Connect Wallet</button>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <button onClick={() => logout()}>Disconnect Wallet</button>
+      <IonPhaser game={game} initialize={true} />
+    </>
+  );
+};
+
+export default App;
