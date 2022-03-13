9ca1123c4bf0c774a5ab14dd21d03ecd30101754
Cristos <cristos@pop-os.localdomain>
2022-03-13 15:01:03 +0100

    Add nft button

--- src/game/scenes/Garage.js
+++ src/game/scenes/Garage.js
@@ -1,6 +1,7 @@
 import Phaser from "phaser";
 import background from '../assets/images/garage-background.png'
 import startRaceBtn from "../assets/images/buttons/start_race_button.png"
+import mintRaceBtn from "../assets/images/buttons/mint_car_button.png"
 
 
 
@@ -13,18 +14,30 @@ export class Garage extends Phaser.Scene {
     preload() {
         this.load.image('garage-background', background);
         this.textures.addBase64('button-start-race', startRaceBtn);
+        this.textures.addBase64('button-mint-car', mintRaceBtn);
     }
 
     create() {
-        this. canvas = this.sys.canvas;
-        console.log(this.sys.canvas)
-        this.add.image(this.canvas.width/2, this.canvas.height/2, 'garage-background');
+        this.canvas = this.sys.canvas;
+        console.log(this.sys);
+
+        this.add.image(this.canvas.width / 2, this.canvas.height / 2, 'garage-background');
+
+        // Car Image
         const carImage = this.add.image(700, 320, 'cars', 'car_blue_1');
         carImage.setAngle(90).setScale(1.3);
-        const startRaceButton = this.add.image(this.canvas.width/2, this.canvas.height- 64, 'button-start-race');
-        startRaceButton.setInteractive();
 
+        // BUTTONS
+        // START RACE
+        const startRaceButton = this.add.image(this.canvas.width / 2, this.canvas.height - 64, 'button-start-race');
+        startRaceButton.setInteractive();
         startRaceButton.on('pointerdown', () => { console.log('pointerdown'); this.scene.start('game') });
+
+        // MINT CAR
+
+        const mintCarButton = this.add.image(700, 490, 'button-mint-car');
+        mintCarButton.setInteractive();
+        mintCarButton.on('pointerdown', () => { console.log('pointerup'); alert("NFT MINTED") });
 
     }
 
